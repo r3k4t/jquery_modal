571acea3ce785e5244530539fdfdfe34086bec19
Rahat Khan Tusar <gb229855@gmail.com>
2022-04-27 16:20:18 +0600

    jQuery Modal

---
+++ js/jquery.modal.js
@@ -0,0 +1,13 @@
+$(function () {
+    $("button").click(function () {
+        $(".div,.close-btn").fadeIn()
+
+    })
+
+    $("a").click(function () {
+        $(".div,.close-btn").fadeOut()
+
+    })
+
+
+})
